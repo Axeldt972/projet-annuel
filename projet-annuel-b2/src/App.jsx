b70b2b15858dd41cc21fbd5c0445cf9ea5195799
Axeldt972 <axel.duquet34@gmail.com>
2025-06-02 11:23:04 -0400

    base

---
+++ projet-annuel-b2/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <h1 class="text-5xl font-bold underline">
+          coucou
+        </h1>
+    </>
+  )
+}
+
+export default App
